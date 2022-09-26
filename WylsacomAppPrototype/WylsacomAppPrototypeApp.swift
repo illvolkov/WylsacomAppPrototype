9de6cc77c158eeb8c9e9c42aa965235cd6af26d2
Ilya Volkov <illvolkov@bk.ru>
2022-09-26 20:00:10 +0500

    Initial Commit

---
+++ WylsacomAppPrototype/WylsacomAppPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WylsacomAppPrototypeApp.swift
+//  WylsacomAppPrototype
+//
+//  Created by Ilya Volkov on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WylsacomAppPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
